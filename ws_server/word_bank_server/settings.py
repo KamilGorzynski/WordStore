61cdbbcd754f92b50477363f7ced81d007c1e32b
Kamil Local <kamil.gorzynski@onet.pl>
2020-07-20 17:55:46 +0200

    Remove secret key

--- ws_server/word_bank_server/settings.py
+++ ws_server/word_bank_server/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-tyvbs2gmm#ckyk@a2-*dkgy&=_k@w^o3wzwlkyyrmq5(&fs%u'
+SECRET_KEY = 'secret_key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
